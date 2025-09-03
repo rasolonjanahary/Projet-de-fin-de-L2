c27172fe1172a3a2628cc279fec3042b4457d7d5
bernardin <rasolonjanahary1263@gmail.com>
2025-09-03 11:19:11 +0200

    Ajout du modéle de regression sur web

--- web/app.py
+++ web/app.py
@@ -21,12 +21,17 @@ app = FastAPI(title="Prédicteur de chance d'admission des étudiants")
 templates = Jinja2Templates(directory="templates")
 
 model = joblib.load("../models/logistic.pkl")
+model_reg = joblib.load("../models/linear.pkl")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/form_cla", response_class=HTMLResponse)
 async def form_page(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
-@app.post("/predict", response_class=HTMLResponse)
+@app.get("/form_reg", response_class=HTMLResponse)
+async def form_page(request: Request):
+    return templates.TemplateResponse("form1.html", {"request": request})
+
+@app.post("/predict_cla", response_class=HTMLResponse)
 async def predire_chance_admission(
     request: Request,
     GRE_Score: float = Form(...),
@@ -64,7 +69,43 @@ async def predire_chance_admission(
         }
     )
 
-#if int(prediction) == 1 else proba[0] * 100
+
+@app.post("/predict_reg", response_class=HTMLResponse)
+async def predire_chance_admission_reg(
+    request: Request,
+    GRE_Score: float = Form(...),
+    TOEFL_Score: float = Form(...),
+    University_Rating: int = Form(...),
+    SOP: float = Form(...),
+    LOR: float = Form(...),
+    CGPA: float = Form(...),
+    Research: int = Form(...)
+    ):
+
+    input_data = {
+        "GRE Score": GRE_Score,
+        "TOEFL Score": TOEFL_Score,
+        "University Rating": University_Rating,
+        "SOP": SOP,
+        "LOR ": LOR,
+        "CGPA": CGPA,
+        "Research": Research
+    }
+    
+    input_df = pd.DataFrame([input_data])
+    
+    # Prédiction
+    prediction = model_reg.predict(input_df)[0]
+
+    return templates.TemplateResponse(
+        "resultat1.html",
+        {
+            "request": request,
+            "prediction": round(float(prediction), 2) * 100,
+            "prediction_c": round(1 - round(float(prediction), 2), 2) * 100,
+            "message": "Vous êtes admis" if prediction == 1 else "Vous n'êtes pas admis"
+        }
+    )
 
 if __name__ == "__main__":
     import uvicorn
